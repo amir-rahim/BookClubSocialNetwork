f9a2d56d85f5b897da0407927e1d7b3f1cda1fe3
Amir Rahim <amir.rahim@kcl.ac.uk>
2022-02-02 01:51:59 +0000

    Update Login Prohibited Home Page

--- BookClub/views/static.py
+++ BookClub/views/static.py
@@ -1,9 +1,6 @@
 '''Static Related Views'''
 from django.shortcuts import render
 
-from BookClub.helpers import login_prohibited
-
-@login_prohibited
 def home(request):
     """This is the home view of the application."""
     return render(request, 'home.html')
