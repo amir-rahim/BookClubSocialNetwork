bbbc7481001d124e062554dff458263488ac51ca
Amir Rahim <amir.rahim@kcl.ac.uk>
2022-02-02 22:45:38 +0000

    Update URLs

--- BookClubSocialNetwork/urls.py
+++ BookClubSocialNetwork/urls.py
@@ -20,5 +20,8 @@ from BookClub import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('login', views.log_in, name='login'),
+    path('sign_up', views.sign_up, name='sign_up'),
+    path('log_out', views.log_out, name='log_out'),
 ]
