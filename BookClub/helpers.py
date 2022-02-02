78fadfab9c680778698141904cb4ca7dd088577d
Amir Rahim <amir.rahim@kcl.ac.uk>
2022-02-02 00:42:59 +0000

    Create Login Prohibited Helper

---
+++ BookClub/helpers.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.shortcuts import redirect
+
+def login_prohibited(view_function):
+    def modified_view_function(request):
+        if request.user.is_authenticated:
+            return redirect(settings.REDIRECT_URL_WHEN_LOGGED_IN)
+        else:
+            return view_function(request)
+    return modified_view_function
