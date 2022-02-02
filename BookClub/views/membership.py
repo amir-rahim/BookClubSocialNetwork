29916583c7c490235ebbdf7a41e9b11a57f2e11f
Jessy BRIARD <jessy.briard@kcl.ac.uk>
2022-02-02 16:59:24 +0100

    Add placeholder templates/available_clubs.html template

--- BookClub/views/membership.py
+++ BookClub/views/membership.py
@@ -2,7 +2,16 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+#from clubs.models import Membership, Club
 
 @login_required
 def available_clubs(request):
-    pass
+    """Show a list of all clubs the user can apply to (all clubs the user is not member of)."""
+    # Select clubs the user is not a member of
+    """subquery = Membership.objects.filter(user=request.user.pk, club=OuterRef('pk'))
+    clubs = Club.objects.filter(
+        ~Q(Exists(subquery)) |
+        Q(Exists(subquery.filter(user_type=Membership.UserTypes.NON_MEMBER)))
+    )
+    return render(request, 'available_clubs.html', {'clubs': clubs})"""
+    return render(request, 'available_clubs.html')
