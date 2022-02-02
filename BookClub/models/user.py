4deeb32d91ed52ccf93c568851bd377ff9dd9957
Amir Rahim <amir.rahim@kcl.ac.uk>
2022-02-02 22:41:36 +0000

    Create User Model

---
+++ BookClub/models/user.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from libgravatar import Gravatar
+
+
+class User(AbstractUser):
+    """User model used for authentication."""
+    
+    username = models.CharField(
+        max_length=30,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex='^[A-Za-z0-9]([._-](?![._-])|[A-Za-z0-9])*[A-Za-z0-9]$',
+                message='Usernames may only contain lowercase characters '
+                        'and . _ - but not as '
+                        'the first or last character.',
+                code='invalid_username'
+            )
+        ]
+    )
+    
+    email = models.EmailField(unique=True, blank=False)
+    public_bio = models.CharField(max_length=250, blank=False)
+    
+    def gravatar(self, size=120):
+        """Return a URL to the user's gravatar."""
+        gravatar_object = Gravatar(self.email)
+        gravatar_url = gravatar_object.get_image(size=size, default='mp')
+        return gravatar_url
