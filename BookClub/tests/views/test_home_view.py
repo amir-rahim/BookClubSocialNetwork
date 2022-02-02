05e7fb93e76a00dc4579fbc5e96115cd952f7a7a
Amir Rahim <amir.rahim@kcl.ac.uk>
2022-02-02 01:24:17 +0000

    Test Home View

---
+++ BookClub/tests/views/test_home_view.py
@@ -0,0 +1,18 @@
+"""Tests of the home view"""
+from django.test import TestCase
+from django.urls import reverse
+
+class HomeViewTestCase(TestCase):
+    """Tests of the home view"""
+
+    def setUp(self):
+        self.url = reverse('home')
+
+    def test_home_url(self):
+        self.assertEqual(self.url, '/')
+
+    def test_get_home(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
