8e089b4a3c174481ae14abc2de3f7e8119b5d52f
Jessy BRIARD <jessy.briard@kcl.ac.uk>
2022-02-04 14:25:18 +0100

    Revert "Query non-joined clubs and pass to HTML template"
    This reverts commit dbb832cdbddd0ba71e6a9a77b4183615a1463442.

--- BookClub/views/membership.py
+++ BookClub/views/membership.py
@@ -2,16 +2,16 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from BookClub.models.club_membership import ClubMembership
-from BookClub.models.club import Club
+#from clubs.models import Membership, Club
 
 @login_required
 def available_clubs(request):
     """Show a list of all clubs the user can apply to (all clubs the user is not member of)."""
     # Select clubs the user is not a member of
-    subquery = ClubMembership.objects.filter(user=request.user.pk, club=OuterRef('pk'))
+    """subquery = Membership.objects.filter(user=request.user.pk, club=OuterRef('pk'))
     clubs = Club.objects.filter(
         ~Q(Exists(subquery)) |
-        Q(Exists(subquery.filter(membership=ClubMembership.UserRoles.APPLICANT)))
+        Q(Exists(subquery.filter(user_type=Membership.UserTypes.NON_MEMBER)))
     )
-    return render(request, 'available_clubs.html', {'clubs': clubs})
+    return render(request, 'available_clubs.html', {'clubs': clubs})"""
+    return render(request, 'available_clubs.html')
