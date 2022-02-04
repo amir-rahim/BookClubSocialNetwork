ff2f8c1bc71f92fdccb6495d2f4493a657db29ea
clerav <ravshanbek.rozukulov@kcl.ac.uk>
2022-02-04 04:23:48 +0000

    Initial club model with tests without getters

--- BookClub/admin.py
+++ BookClub/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from BookClub.models import User
+from BookClub.models.user import User
+from BookClub.models.club import Club
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
@@ -7,4 +8,12 @@ class UserAdmin(admin.ModelAdmin):
 
     list_display = [
         'id','username', 'email'
+    ]
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for clubs."""
+
+    list_display = [
+        'name','description', 'is_private', 'rules', 'created_on'
     ]
