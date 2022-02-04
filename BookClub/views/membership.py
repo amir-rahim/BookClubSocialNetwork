dbb832cdbddd0ba71e6a9a77b4183615a1463442
Jessy BRIARD <jessy.briard@kcl.ac.uk>
2022-02-04 14:11:38 +0100

    Query non-joined clubs and pass to HTML template

--- BookClub/views/membership.py
+++ BookClub/views/membership.py
@@ -2,16 +2,16 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-#from clubs.models import Membership, Club
+from BookClub.models.club_membership import ClubMembership
+from BookClub.models.club import Club
 
 @login_required
 def available_clubs(request):
     """Show a list of all clubs the user can apply to (all clubs the user is not member of)."""
     # Select clubs the user is not a member of
-    """subquery = Membership.objects.filter(user=request.user.pk, club=OuterRef('pk'))
+    subquery = ClubMembership.objects.filter(user=request.user.pk, club=OuterRef('pk'))
     clubs = Club.objects.filter(
         ~Q(Exists(subquery)) |
-        Q(Exists(subquery.filter(user_type=Membership.UserTypes.NON_MEMBER)))
+        Q(Exists(subquery.filter(membership=ClubMembership.UserRoles.APPLICANT)))
     )
-    return render(request, 'available_clubs.html', {'clubs': clubs})"""
-    return render(request, 'available_clubs.html')
+    return render(request, 'available_clubs.html', {'clubs': clubs})
