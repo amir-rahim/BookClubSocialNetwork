4101025ce8e35ddfa234815e0ac9abe30c991f56
Jack Curtis <jack.curtis@kcl.ac.uk>
2022-02-05 23:01:48 +0000

    Add RankRequiredMixin

--- BookClub/helpers.py
+++ BookClub/helpers.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.shortcuts import redirect
+from BookClub.models.club_membership import ClubMembership
+from django.db.models import Q
 class LoginProhibitedMixin:
     """
         If user trying to access this view is authenticated, they are redirected to the 'home' page
@@ -8,3 +10,21 @@ class LoginProhibitedMixin:
         if self.request.user.is_authenticated:
             return redirect('home')
         return super().dispatch(request, *args, **kwargs)
+
+class RankRequiredMixin:
+    """
+    If user is trying to access this view has required rank and is in required club, they continue, otherwise they are redirected to home page
+    """
+    requiredRanking = ClubMembership.UserRoles.OWNER
+    requiredClub = -1
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            club_membership = ClubMembership.objects.get(Q(club=self.requiredClub)&Q(user=request.user))
+        except ClubMembership.DoesNotExist:
+            return redirect('home')
+        if club_membership is not None:
+            if club_membership.membership == self.requiredRanking:
+                return super().dispatch(request, *args, **kwargs)
+        else:
+            return redirect('home')
+        
