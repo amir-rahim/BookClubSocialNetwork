20a25fccffb21e4dc857c65800b1b4cccfa8ff6b
Jack Curtis <jack.curtis@kcl.ac.uk>
2022-02-07 19:20:54 +0000

    Add LOGIN_URL

--- BookClubSocialNetwork/settings.py
+++ BookClubSocialNetwork/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+LOGIN_URL = 'login'
 REDIRECT_URL_WHEN_LOGGED_IN = 'home'
 
 AUTH_USER_MODEL = 'BookClub.User'
