8097c4707239f0384e443b3895dfdddcf48f03d5
Zafi Shah <shah.zafira@gmail.com>
2022-02-08 01:56:30 +0000

    Update test_club_dashboard_view
    Update to fit new fixtures

--- BookClub/tests/views/test_club_dashboard_view.py
+++ BookClub/tests/views/test_club_dashboard_view.py
@@ -1,8 +1,8 @@
 """Unit tests of the club dashboard view."""
 from django.test import TestCase
-from BookClub.models import Club, Membership
+from BookClub.models import Club, User, Membership
 from django.urls import reverse
-from BookClub.tests.helpers import reverse_with_next
+from BookClub.helpers import reverse_with_next
 
 
 class ClubDashboardViewTest(TestCase):
@@ -14,9 +14,9 @@ class ClubDashboardViewTest(TestCase):
     ]
 
     def setUp(self):
-        self.user = User.objects.get(email="johndoe@example.com")
-        self.jane = User.objects.get(email="janedoe@example.com")
-        self.club = Club.objects.get(name="Lit Masterpiece")
+        self.user = User.objects.get(username="johndoe")
+        self.jane = User.objects.get(username="janedoe")
+        self.club = Club.objects.get(name="Johnathan's Club")
         self.url = reverse("club_dashboard", kwargs={"club_id": self.club.id})
         self.owner = self.club.get_owner()
 
@@ -24,18 +24,19 @@ class ClubDashboardViewTest(TestCase):
         self.assertEqual(self.url, f"/club_dashboard/")
 
     def test_club_dashboard_has_club_info(self):
-        self.client.login(username=self.user.email, password="Password123")
+        self.client.login(username=self.user.username, password="Password123")
         url = reverse("club_dashboard", kwargs={"club_id": self.club.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "club_dashboard.html")
-        self.assertContains(response, "Lit Masterpiece")
-        self.assertContains(response, "This is the first club")
-        self.assertContains(response, "2022-02-06")
-        self.assertContains(response, "Keep reading")
+        self.assertContains(response, "Johnathan's Club")
+        self.assertContains(response, "This is a very cool club that is owned by a certain Johnathan. Reading certain books...")
+        self.assertContains(response, "Welcome to Johnathan's club! We read the best books!!!")
+        self.assertContains(response, "Don't be annoying")
+        self.assertContains(response, "2022-02-04")
 
     def test_club_dashboard_has_club_stats(self):
-        self.client.login(username=self.user.email, password="Password123")
+        self.client.login(username=self.user.username, password="Password123")
         url = reverse("club_dashboard", kwargs={"club_id": self.club.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
@@ -46,10 +47,10 @@ class ClubDashboardViewTest(TestCase):
         self.assertContains(response, self.club.get_review_score())
 
     def test_club_dashboard_has_owner_info(self):
-        self.client.login(username=self.user.email, password="Password123")
+        self.client.login(username=self.user.username, password="Password123")
         url = reverse("club_dashboard", kwargs={"club_id": self.club.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "club_dashboard.html")
-        self.assertTemplateUsed(response, "janedoe")
+        self.assertContains(response, "janedoe")
         self.assertContains(response, "janedoe@example.com")
