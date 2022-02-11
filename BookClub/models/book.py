d2f6a1c4be884c4b0bb4c2c2b9e1d6030ba55e4c
Jack Curtis <jack.curtis@kcl.ac.uk>
2022-02-11 13:56:32 +0000

    Add book, booklist models. Add migrations, add book model fixture and testing.

--- BookClub/models/book.py
+++ BookClub/models/book.py
@@ -7,9 +7,9 @@ class Book(models.Model):
     
     title = models.CharField(unique=False, max_length=200, blank=False)
     ISBN = models.CharField(unique=True, max_length=15,blank=False)
-    author = models.CharField(unique=False, blank=False)
+    author = models.CharField(unique=False, blank=False, max_length=100)
     publicationYear = models.DateField(unique=False, blank=False)
-    publisher = models.CharField(unique=False, blank=False)
+    publisher = models.CharField(unique=False, blank=False, max_length=100)
     imageS = models.URLField(unique=False, blank=True)
     imageM = models.URLField(unique=False, blank=True)
     imageL = models.URLField(unique=False, blank=True)
@@ -18,6 +18,5 @@ class Book(models.Model):
         return self.title
     
     def getPublicationYear(self):
-        date = datetime(self.publicationYear)
+        date = self.publicationYear
         return date.year
-        
--- BookClub/models/booklist.py
+++ BookClub/models/booklist.py
@@ -4,6 +4,8 @@ from BookClub.models import User, Book
 
 
 class BookList(models.Model):
+    title = models.CharField(unique=False, blank=False, max_length=120)
+    description = models.CharField(unique=False, blank=True, max_length=240)
     creator = models.ForeignKey(User,blank= False, null = False, on_delete= models.CASCADE)
     books = models.ManyToManyField(Book, on_delete=models.RESTRICT, blank=True, unique=True)
     created_on = models.DateTimeField(auto_now_add=True)
