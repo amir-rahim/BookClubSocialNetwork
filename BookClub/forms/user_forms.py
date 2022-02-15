9aa7001f3a25499cb8f7a54d5da49f815cd5c4be
Amir Rahim <74428555+amir-rahim@users.noreply.github.com>
2022-02-15 17:12:21 +0000

    Update user_forms.py

--- BookClub/forms/user_forms.py
+++ BookClub/forms/user_forms.py
@@ -12,28 +12,6 @@ class EditProfileForm(forms.ModelForm):
         fields = ['username', 'email', 'public_bio']
         widgets = { 'public_bio': forms.Textarea() }
 
-# class ChangePasswordForm(forms.Form):
-#     """Form enabling users to change their password."""
-#     password = forms.CharField(label='Current password', widget=forms.PasswordInput())
-#     new_password = forms.CharField(
-#         label='Password',
-#         widget=forms.PasswordInput(),
-#         validators=[RegexValidator(
-#             regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$',
-#             message='Password must contain an uppercase character, a lowercase '
-#                     'character and a number'
-#             )]
-#     )
-#     password_confirmation = forms.CharField(label='Password confirmation', widget=forms.PasswordInput())
-
-#     def clean(self):
-#         """Clean the data and generate messages for any errors."""
-#         super().clean()
-#         new_password = self.cleaned_data.get('new_password')
-#         password_confirmation = self.cleaned_data.get('password_confirmation')
-#         if new_password != password_confirmation:
-#             self.add_error('password_confirmation', 'Confirmation does not match password.')
-
 class NewPasswordMixin(forms.Form):
     """Form mixing for new_password and password_confirmation fields."""
 
