379ee36fa51832063b5f9a377be1bfadf5453b11
Raymond Chung <k20052048@kcl.ac.uk>
2022-02-16 23:17:14 +0000

    Squashed commit of the following:
    commit e9f6f3444dfbc9b2b3a77e44ae916206c7f8a857
    Author: Zafi Shah <shah.zafira@gmail.com>
    Date:   Tue Feb 15 20:23:28 2022 +0000
    
        Test Meeting model
    
    commit 9398b28d9a8ed513f58e55c3f0b4c38292577ade
    Author: Zafi Shah <shah.zafira@gmail.com>
    Date:   Tue Feb 15 20:22:09 2022 +0000
    
        Create meeting model
    
    commit 161119cde0d193506de5ab54326a939001caab1c
    Merge: af6c284 ac09203
    Author: Zafi Shah <shah.zafira@gmail.com>
    Date:   Tue Feb 15 18:48:00 2022 +0000
    
        Merge remote-tracking branch 'origin/BooksAndLists' into Meeting
    
    commit ac09203b390d38f2b06aa71713a5c5642985574d
    Author: Jack Curtis <jack.curtis@kcl.ac.uk>
    Date:   Fri Feb 11 14:54:41 2022 +0000
    
        Book migrations
    
    commit 77f47511f05e563fc02708b97d542b9eaf768690
    Author: Jack Curtis <jack.curtis@kcl.ac.uk>
    Date:   Fri Feb 11 14:54:22 2022 +0000
    
        Add addbooks boilerplate and BookList to init.py
    
    commit d2f6a1c4be884c4b0bb4c2c2b9e1d6030ba55e4c
    Author: Jack Curtis <jack.curtis@kcl.ac.uk>
    Date:   Fri Feb 11 13:56:32 2022 +0000
    
        Add book, booklist models. Add migrations, add book model fixture and testing.
    
    commit 4105ecc03be081d74a500d998e8a79ce955e80bc
    Author: Jack Curtis <jack.curtis@kcl.ac.uk>
    Date:   Thu Feb 10 22:03:39 2022 +0000
    
        Create Book and Booklist models. Update requirements.txt. Begin testing for test_book_model.

---
+++ BookClub/models/booklist.py
@@ -0,0 +1,16 @@
+from django.db import models
+from BookClub.models import User, Book
+
+
+
+class BookList(models.Model):
+    title = models.CharField(unique=False, blank=False, max_length=120)
+    description = models.CharField(unique=False, blank=True, max_length=240)
+    creator = models.ForeignKey(User,blank= False, null = False, on_delete=models.CASCADE)
+    books = models.ManyToManyField(Book, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    
+    def addBooks(self):
+        #TODO constraint not allowing duplicate books
+        pass
---
+++ BookClub/tests/models/test_meeting_model.py
@@ -0,0 +1,129 @@
+from django.test import TestCase
+from django.utils import timezone
+import pytz
+
+from BookClub.models import Meeting, Club, User, Book
+# from BookClub.models.club_membership import ClubMembership
+from django.core.exceptions import ValidationError
+
+class MeetingTestCase(TestCase):
+
+    fixtures = [
+        'BookClub/tests/fixtures/books.json',
+        'BookClub/tests/fixtures/default_users.json',
+        'BookClub/tests/fixtures/default_clubs.json',
+        'BookClub/tests/fixtures/default_meetings.json'
+    ]
+
+    def setUp(self):
+        self.club = Club.objects.get(pk = 1)
+        self.meeting = Meeting.objects.get(pk = 1)
+        self.meeting.meeting_time = timezone.now()
+        self.user = User.objects.get(username = 'johndoe')
+        self.jack = User.objects.get(username = "jackdoe")
+    
+    def _assert_meeting_is_valid(self):
+        try:
+            self.meeting.full_clean()
+        except(ValidationError):
+            self.fail('test meeting should be valid')
+        
+    def _assert_meeting_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.meeting.full_clean()
+
+    def test_valid_club(self):
+        self._assert_meeting_is_valid()
+
+# Field testing
+
+    # Organiser testing
+
+    def test_organiser_cannot_be_blank(self):
+        self.meeting.organiser = None
+        self._assert_meeting_is_invalid()
+
+    # Club testing
+
+    def test_club_cannot_be_blank(self):
+        self.meeting.club = None
+        self._assert_meeting_is_invalid()
+
+    # Meeting time testing
+
+    def test_meeting_time_cannot_be_blank(self):
+        self.meeting.meeting_time = ''
+        self._assert_meeting_is_invalid()
+
+    # Location testing
+
+    def test_location_can_be_120_long(self):
+        self.meeting.location = 'x' * 120
+        self._assert_meeting_is_valid()
+
+    def test_location_cannot_be_over_120_long(self):
+        self.meeting.location = 'x' * 121
+        self._assert_meeting_is_invalid()
+
+    def test_location_can_be_blank(self):
+        self.meeting.location = ''
+        self._assert_meeting_is_valid()
+
+    # Title testing
+
+    def test_title_can_be_120_long(self):
+        self.meeting.title = 'x' * 120
+        self._assert_meeting_is_valid()
+
+    def test_title_cannot_be_over_120_long(self):
+        self.meeting.title = 'x' * 121
+        self._assert_meeting_is_invalid()
+
+    def test_title_cannot_be_blank(self):
+        self.meeting.title = ''
+        self._assert_meeting_is_invalid()
+
+    # Description testing
+
+    def test_description_can_be_250_long(self):
+        self.meeting.description = 'x' * 250
+        self._assert_meeting_is_valid()
+
+    def test_description_cannot_be_over_250_long(self):
+        self.meeting.description = 'x' * 251
+        self._assert_meeting_is_invalid()
+
+    def test_description_cannot_be_blank(self):
+        self.meeting.description = ''
+        self._assert_meeting_is_invalid()
+
+    # Members testing
+
+    def test_member_can_join(self):
+        self.meeting.join_member(self.jack)
+        self.assertEquals(self.meeting.get_members().count(), 3)
+        self._assert_meeting_is_valid()
+
+    def test_member_can_leave(self):
+        self.meeting.leave_member(self.user)
+        self.assertEquals(self.meeting.get_members().count(), 1)
+        self._assert_meeting_is_valid()
+
+
+    # Type testing
+
+    def test_meeting_type_enum_is_invalid(self):
+        self.meeting.type = 'x'
+        self._assert_meeting_is_invalid()
+
+    def test_meeting_type_enum_is_valid(self):
+        self.meeting.type = 'B'
+        self._assert_meeting_is_valid()
+
+    # Book testing
+
+    def test_book_can_be_blank(self):
+        self.meeting.book = None
+        self._assert_meeting_is_valid()
+        
+
