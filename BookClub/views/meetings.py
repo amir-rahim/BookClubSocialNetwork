0f6fd1b8941fe20117206c4697bf7d3094c51963
Marsha Lau <k20036179@kcl.ac.uk>
2022-02-17 22:59:04 +0000

    Fix views

--- BookClub/views/meetings.py
+++ BookClub/views/meetings.py
@@ -1,20 +1,16 @@
 from django.views.generic import View
 from BookClub.helpers import *
-from BookClub.models.club import Club
-from BookClub.models.meeting import Meeting
+from BookClub.models import Meeting
 from BookClub.helpers import *
-from BookClub.models.club import Club
-from BookClub.models.meeting import Meeting
 from django.shortcuts import redirect
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-import datetime
-
+from django.utils import timezone
 
 class JoinMeetingView(LoginRequiredMixin, View):
     """Users can join meetings"""
 
-    redirect_location = '' # list of meetings
+    redirect_location = 'home' # should be list of meetings
 
     def get(self, request, *args, **kwargs):
         return redirect(self.redirect_location)
@@ -22,7 +18,7 @@ class JoinMeetingView(LoginRequiredMixin, View):
     def is_actionable(self, currentUser, meeting):
         """Check if user can join a meeting"""
 
-        return (not meeting.get_members().filter(username = currentUser.username).exists()) and meeting.get_meeting_time() < datetime.now()
+        return (not meeting.get_members().filter(username = currentUser.username).exists()) and meeting.get_meeting_time() > timezone.now()
 
     def is_not_actionable(self):
         """If user cannot join meeting"""
@@ -32,9 +28,8 @@ class JoinMeetingView(LoginRequiredMixin, View):
     def action(self, currentUser, meeting):
         """User joins the meeting"""
 
-        meeting.join_member(self, currentUser)
         messages.success(self.request, "You have joined the meeting.")
-
+        meeting.join_member(currentUser)
 
     def post(self, request, *args, **kwargs):
 
@@ -48,23 +43,22 @@ class JoinMeetingView(LoginRequiredMixin, View):
         if (self.is_actionable(currentUser, meeting)):
             self.action(currentUser, meeting)
         else:
-            self.is_not_actionable(currentUser, meeting)
+            self.is_not_actionable()
 
         return redirect(self.redirect_location)
-
 
 class LeaveMeetingView(LoginRequiredMixin, View):
     """Users can leave meetings"""
 
-    redirect_location = '' # list of meetings
+    redirect_location = 'home' # list of meetings
 
     def get(self, request, *args, **kwargs):
         return redirect(self.redirect_location)
 
     def is_actionable(self, currentUser, meeting):
-        """Check if user can leave meetings"""
+        """Check if user can leave a meeting"""
 
-        return (meeting.get_members().filter(username = currentUser.username).exists()) and (meeting.get_organiser() != currentUser) and (meeting.get_meeting_time() > datetime.now())
+        return (meeting.get_members().filter(username = currentUser.username).exists()) and (meeting.get_organiser() != currentUser) and meeting.get_meeting_time() > timezone.now()
 
     def is_not_actionable(self):
         """If user cannot leave meeting"""
@@ -72,10 +66,10 @@ class LeaveMeetingView(LoginRequiredMixin, View):
         return messages.info(self.request, "You cannot leave this meeting.")
 
     def action(self, currentUser, meeting):
-        """Leave meeting"""
+        """User leave the meeting"""
 
-        meeting.leave_member(self, currentUser)
         messages.success(self.request, "You have left the meeting.")
+        meeting.leave_member(currentUser)
 
     def post(self, request, *args, **kwargs):
 
@@ -89,6 +83,6 @@ class LeaveMeetingView(LoginRequiredMixin, View):
         if (self.is_actionable(currentUser, meeting)):
             self.action(currentUser, meeting)
         else:
-            self.is_not_actionable(currentUser, meeting)
+            self.is_not_actionable()
 
         return redirect(self.redirect_location)
