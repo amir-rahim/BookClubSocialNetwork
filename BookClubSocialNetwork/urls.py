9b5d870d442fb86472325e699d657e4d61da1093
Amir Rahim <amir.rahim@kcl.ac.uk>
2022-02-18 19:28:18 +0000

    Implement Favicon

--- BookClubSocialNetwork/urls.py
+++ BookClubSocialNetwork/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from BookClub import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 # from BookClub.views.action_views import DemoteMemberView, PromoteView
 
@@ -24,6 +26,7 @@ urlpatterns = [
     # '''Core URLs'''
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'))),
 
 
     # '''Authentication URLs'''
