8eb84e935e5e9b7850f36f8a51672850e1c6f6a4
clerav <ravshanbek.rozukulov@kcl.ac.uk>
2022-02-18 01:21:46 +0000

    Add BookList testing

--- BookClub/models/booklist.py
+++ BookClub/models/booklist.py
@@ -6,11 +6,13 @@ from BookClub.models import User, Book
 class BookList(models.Model):
     title = models.CharField(unique=False, blank=False, max_length=120)
     description = models.CharField(unique=False, blank=True, max_length=240)
-    creator = models.ForeignKey(User,blank= False, null = False, on_delete=models.CASCADE)
-    books = models.ManyToManyField(Book, blank=True)
+    creator = models.ForeignKey(User, blank= False, null = False, on_delete=models.CASCADE)
+    books = models.ManyToManyField(Book)
     created_on = models.DateTimeField(auto_now_add=True)
-    
-    
-    def addBooks(self):
-        pass
-        #TODO constraint not allowing duplicate books
+
+
+    # def addBooks(self, *new_books):
+    #     return self.books.add(new_books)
+
+    # Django ManyToManyField acts as a set, and ignores duplicates automatically.
+    #   Therefore, we can directly use .add() and .set() methods on books field
