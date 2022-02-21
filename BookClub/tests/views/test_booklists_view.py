67b42f7824e34404d2507f8c511725d72aad2de9
clerav <ravshanbek.rozukulov@kcl.ac.uk>
2022-02-21 00:14:32 +0000

    Add testing for booklists_list view

---
+++ BookClub/tests/views/test_booklists_view.py
@@ -0,0 +1,101 @@
+"""Tests of the booklists_list view."""
+from django.test import TestCase, tag
+from django.urls import reverse
+from BookClub.models import User, Club, ClubMembership, Book, BookList
+
+from BookClub.tests.helpers import reverse_with_next
+
+@tag('booklist', 'book', 'user')
+@tag('booklist_view')
+class UserBooklistsViewTestCase(TestCase):
+    """Tests of the booklists_list view."""
+
+    fixtures = [
+        'BookClub/tests/fixtures/default_users.json',
+        'BookClub/tests/fixtures/books.json',
+        'BookClub/tests/fixtures/booklists.json'
+    ]
+
+    def setUp(self):
+        self.user = User.objects.get(username="johndoe")
+        self.url = reverse('booklists_list', kwargs={'username': self.user.username})
+
+    def _get_url_for_user(self, username):
+        return reverse('booklists_list', kwargs={'username': username})
+
+    def test_url(self):
+        self.assertEqual(self.url,f"/user/{self.user.username}/lists")
+
+    # def test_does_not_redirect_when_not_logged_in(self):
+    #     redirect_url = reverse_with_next('login', self.url)
+    #     response = self.client.get(self.url, follow = True)
+    #     self.assertRedirects(response, redirect_url,
+    #         status_code=302, target_status_code=200, fetch_redirect_response=True
+    #     )
+    #     self.assertTemplateUsed(response, 'login.html')
+
+    def test_get_template_logged_in(self):
+        self.client.login(username=self.user.username, password="Password123")
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user_booklists.html')
+
+    def test_contains_lists_created_by_user(self):
+        self.client.login(username = self.user.username, password = 'Password123')
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user_booklists.html')
+        response_lists = list(response.context['booklists'])
+        user_lists = list(BookList.objects.filter(creator = self.user))
+
+        for userlist in user_lists:
+            self.assertIn(userlist, response_lists)
+
+    def test_does_not_contain_lists_created_by_other_users(self):
+        self.client.login(username = self.user.username, password = 'Password123')
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user_booklists.html')
+        response_lists = list(response.context['booklists'])
+        lists_by_other_users = list(BookList.objects.exclude(creator = self.user))
+
+        for userlist in lists_by_other_users:
+            self.assertNotIn(lists_by_other_users, response_lists)
+
+    def test_user_viewing_own_lists_is_recognized(self):
+        self.client.login(username = self.user.username, password = 'Password123')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user_booklists.html')
+        self.assertEqual(response.context['self'], True)
+        self.assertContains(response, '<span>Edit</span>')
+
+    def test_user_viewing_not_own_lists_is_recognized(self):
+        self.client.login(username = self.user.username, password = 'Password123')
+        response = self.client.get(self._get_url_for_user('janedoe'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user_booklists.html')
+        self.assertEqual(response.context['self'], False)
+
+    def test_view_lists_of_another_user_without_lists(self):
+        self.client.login(username = self.user.username, password = 'Password123')
+        response = self.client.get(self._get_url_for_user('jeannettedoe'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user_booklists.html')
+        response_lists = list(response.context['booklists'])
+        self.assertEqual(len(response_lists), 0)
+        self.assertContains(response, '<div class="box">No lists. </div>')
+
+    def test_user_without_lists_viewing_own_lists(self):
+        self.client.login(username = 'jeannettedoe', password = 'Password123')
+        response = self.client.get(self._get_url_for_user('jeannettedoe'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user_booklists.html')
+        response_lists = list(response.context['booklists'])
+        self.assertEqual(len(response_lists), 0)
+        self.assertContains(response, '<div class="box">No lists. <a href="#">Create one?</a></div>')
--- BookClub/views/booklists_view.py
+++ BookClub/views/booklists_view.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.views.generic.list import ListView
 
@@ -13,13 +14,11 @@ class BooklistListView(ListView):
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs['username'])
-        print(f"Found the user: {user.username}")
         return BookList.objects.filter(creator = user)
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        print(f"done context. lists: {str(list(context['booklists']))}")
         # Add in a QuerySet of all the books
         creator = User.objects.get(username = self.kwargs['username'])
         context['creator'] = creator
