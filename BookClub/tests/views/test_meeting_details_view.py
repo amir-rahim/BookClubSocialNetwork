2845c02fc93dfbb3af8245a99ffe972fdf817f9a
Marsha Lau <k20036179@kcl.ac.uk>
2022-02-22 21:21:15 +0000

    Add tests for different authorization
    Co-Authored-By: Zafi Shah <25832155+shahzafira@users.noreply.github.com>

--- BookClub/tests/views/test_meeting_details_view.py
+++ BookClub/tests/views/test_meeting_details_view.py
@@ -25,11 +25,10 @@ class MeetingDetailsViewTestCase(TestCase, LogInTester):
         self.private_club = Club.objects.get(pk='3')
 
         self.url = reverse('meeting_details', kwargs={'club_url_name' : self.club.club_url_name, 'meeting_id' : self.meeting.id})
-        #self.url = '/club/'+self.club.club_url_name+'/meetings/'+str(self.meeting.id)+'/'
-    @tag('url')
+
     def test_url(self):
-        #self.assertEqual(self.url,f'/club/{self.club.club_url_name}/meetings/{self.meeting.id}')
         self.assertEqual(self.url, '/club/'+self.club.club_url_name+'/meetings/'+str(self.meeting.id)+'/')
+    
     def test_redirects_when_not_logged_in(self):
         self.assertFalse(self._is_logged_in())
         response = self.client.get(self.url)
@@ -41,7 +40,7 @@ class MeetingDetailsViewTestCase(TestCase, LogInTester):
         response = self.client.get(reverse("meeting_details", kwargs={"club_url_name": self.private_club.club_url_name, 'meeting_id': self.private_meeting.id}))
         self.assertRedirects(response, expected_url=reverse("available_clubs"), status_code=302, target_status_code=200)
 
-    def test_can_see_meeting_name_and_club(self):
+    def test_user_can_see_meeting_name_and_club(self):
         self.client.login(username=self.user.username, password="Password123")
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
@@ -49,7 +48,21 @@ class MeetingDetailsViewTestCase(TestCase, LogInTester):
         self.assertContains(response, "Book meeting 1")
         self.assertContains(response, "Johnathan Club")
 
-    def test_can_see_meeting_details_book_meeting(self):
+    def test_organiser_can_see_meeting_details_book_meeting(self):
+        self.client.login(username=self.organiser.username, password="Password123")
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "meeting_details.html")
+        self.assertContains(response, "Feb. 22, 2022, 7 p.m.")
+        self.assertContains(response, "Feb. 10, 2022")
+        self.assertContains(response, "Franklin Wilkins Library GS04")
+        self.assertContains(response, "Book meeting 1")
+        self.assertContains(response, "This is a book meeting, helll yeahhhh")
+        self.assertContains(response, "2")
+        self.assertContains(response, "Book")
+        self.assertContains(response, "Classical Mythology")
+
+    def test_user_can_see_meeting_details_book_meeting(self):
         self.client.login(username=self.user.username, password="Password123")
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
@@ -63,7 +76,22 @@ class MeetingDetailsViewTestCase(TestCase, LogInTester):
         self.assertContains(response, "Book")
         self.assertContains(response, "Classical Mythology")
 
-    def test_can_see_details_social_meeting(self):
+    def test_organiser_can_see_details_social_meeting(self):
+        self.client.login(username=self.organiser.username, password="Password123")
+        social_meeting = Meeting.objects.get(pk='5')
+        social_meeting_url = reverse('meeting_details', kwargs={'club_url_name' : self.club.club_url_name, 'meeting_id' : social_meeting.id})
+        response = self.client.get(social_meeting_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "meeting_details.html")
+        self.assertContains(response, "Feb. 22, 2022, 7 p.m.")
+        self.assertContains(response, "Feb. 10, 2022")
+        self.assertContains(response, "Franklin Wilkins Library GS04")
+        self.assertContains(response, "Social meeting 1")
+        self.assertContains(response, "This is a social meeting, helll yeahhhh")
+        self.assertContains(response, "2")
+        self.assertContains(response, "Social")
+
+    def test_user_can_see_details_social_meeting(self):
         self.client.login(username=self.user.username, password="Password123")
         social_meeting = Meeting.objects.get(pk='5')
         social_meeting_url = reverse('meeting_details', kwargs={'club_url_name' : self.club.club_url_name, 'meeting_id' : social_meeting.id})
@@ -78,7 +106,22 @@ class MeetingDetailsViewTestCase(TestCase, LogInTester):
         self.assertContains(response, "2")
         self.assertContains(response, "Social")
 
-    def test_can_see_details_club_meeting(self):
+    def test_organiser_can_see_details_club_meeting(self):
+        self.client.login(username=self.organiser.username, password="Password123")
+        club_meeting = Meeting.objects.get(pk='6')
+        club_meeting_url = reverse('meeting_details', kwargs={'club_url_name' : self.club.club_url_name, 'meeting_id' : club_meeting.id})
+        response = self.client.get(club_meeting_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "meeting_details.html")
+        self.assertContains(response, "Feb. 22, 2022, 7 p.m.")
+        self.assertContains(response, "Feb. 10, 2022")
+        self.assertContains(response, "Franklin Wilkins Library GS04")
+        self.assertContains(response, "Club meeting 1")
+        self.assertContains(response, "This is a club meeting, helll yeahhhh")
+        self.assertContains(response, "2")
+        self.assertContains(response, "Club")
+
+    def test_user_can_see_details_club_meeting(self):
         self.client.login(username=self.user.username, password="Password123")
         club_meeting = Meeting.objects.get(pk='6')
         club_meeting_url = reverse('meeting_details', kwargs={'club_url_name' : self.club.club_url_name, 'meeting_id' : club_meeting.id})
@@ -93,7 +136,22 @@ class MeetingDetailsViewTestCase(TestCase, LogInTester):
         self.assertContains(response, "2")
         self.assertContains(response, "Club")
 
-    def test_can_see_details_other_meeting(self):
+    def test_organiser_can_see_details_other_meeting(self):
+        self.client.login(username=self.organiser.username, password="Password123")
+        other_meeting = Meeting.objects.get(pk='7')
+        other_meeting_url = reverse('meeting_details', kwargs={'club_url_name' : self.club.club_url_name, 'meeting_id' : other_meeting.id})
+        response = self.client.get(other_meeting_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "meeting_details.html")
+        self.assertContains(response, "Feb. 22, 2022, 7 p.m.")
+        self.assertContains(response, "Feb. 10, 2022")
+        self.assertContains(response, "Franklin Wilkins Library GS04")
+        self.assertContains(response, "Other meeting 1")
+        self.assertContains(response, "This is an other meeting, helll yeahhhh")
+        self.assertContains(response, "2")
+        self.assertContains(response, "Other")
+
+    def test_user_can_see_details_other_meeting(self):
         self.client.login(username=self.user.username, password="Password123")
         other_meeting = Meeting.objects.get(pk='7')
         other_meeting_url = reverse('meeting_details', kwargs={'club_url_name' : self.club.club_url_name, 'meeting_id' : other_meeting.id})
@@ -108,7 +166,15 @@ class MeetingDetailsViewTestCase(TestCase, LogInTester):
         self.assertContains(response, "2")
         self.assertContains(response, "Other")
 
-    def test_can_see_organiser_info(self):
+    def test_organiser_can_see_organiser_info(self):
+        self.client.login(username=self.organiser.username, password="Password123")
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "meeting_details.html")
+        self.assertContains(response, "johndoe")
+        self.assertContains(response, "johndoe@example.com")
+
+    def test_user_can_see_organiser_info(self):
         self.client.login(username=self.user.username, password="Password123")
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
