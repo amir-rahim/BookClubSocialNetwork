c957331f284e048a28643f37425a6df04cae8e50
sebmalos <sebastian.malos@kcl.ac.uk>
2022-02-23 00:50:49 +0000

    Clean up the create meeting view
    -small change to create html template

--- BookClub/views/meetings.py
+++ BookClub/views/meetings.py
@@ -5,29 +5,31 @@ from django.views.generic.edit import CreateView
 from django.shortcuts import reverse, redirect
 from django.urls import reverse_lazy
 from BookClub.forms.meeting import MeetingForm
-from BookClub.models.club import Club
-from BookClub.models.user import User
-from BookClub.models.club_membership import ClubMembership
-from BookClub.models.meeting import Meeting
+from BookClub.models import User, Club, ClubMembership, Meeting
 from BookClub.helpers import *
 
 
 class CreateMeetingView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
-    template_name = 'create_meeting.html'
+    '''Class for club owners and moderators creating meetings for the club'''
+
     model = Meeting
     form_class = MeetingForm
-    success_url = '/'
+    template_name = 'create_meeting.html'
 
     # Redirect if user is not a moderator or owner of the club
     def test_func(self):
         try:
-            current_club = Club.objects.get(club_url_name=self.kwargs['club_url_name'])
-            if not (has_owner_rank(self.request.user, current_club) or has_moderator_rank(self.request.user, current_club)):
-                messages.add_message(self.request, messages.ERROR, 'You are not allowed to create meetings!')
-                return False
+            club = Club.objects.get(club_url_name=self.kwargs['club_url_name'])
+            if not (has_owner_rank(self.request.user, club) or has_moderator_rank(self.request.user, club)):
+                if club.is_private == False or has_member_rank(self.request.user, club):
+                    messages.error(self.request, 'Only the owner and moderators can create meetings!')
+                    return False
+                else:
+                    return False
             else:
                 return True
         except:
+            messages.error(self.request, 'Club not found or you are not a member of this club')
             return False
 
     def handle_no_permission(self):
@@ -38,16 +40,21 @@ class CreateMeetingView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
 
     def form_valid(self, form):
         club = Club.objects.get(club_url_name=self.kwargs['club_url_name'])
+        # meeting = form.save(commit=False)
         form.instance.organiser = self.request.user
         form.instance.club = club
-        form.save()
+        form.instance.save()
         form.instance.join_member(self.request.user)
-        messages.success(self.request, ('Successfully created a new meeting!'))
-        return super().form_valid(form)
+        messages.success(self.request, 'Successfully created a new meeting!')
+        return super(CreateMeetingView, self).form_valid(form)
 
     def form_invalid(self, form):
-        messages.add_message(self.request, messages.ERROR, "The data provided was invalid!")
+        messages.error(self.request, "The data provided was invalid!")
         return super().form_invalid(form);
+
+    def get_success_url(self):
+        # Change to redirect to list of meetings
+        return reverse('create_meeting', kwargs={'club_url_name': self.kwargs['club_url_name']})
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
