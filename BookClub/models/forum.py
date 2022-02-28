610bc5f102f7ae953fca870eac062ddd1a79cdac
Jack Curtis <jack.curtis@kcl.ac.uk>
2022-02-28 02:38:41 +0000

    Add the add and remove methods to ForumPost, ForumComment and Forum

--- BookClub/models/forum.py
+++ BookClub/models/forum.py
@@ -8,9 +8,25 @@ class ForumPost(TextPost):
         
     comments = models.ManyToManyField('ForumComment', related_name='Comments', blank=True)
     
+    def add_comment(self, comment):
+        self.comments.add(comment)
+        self.save()
+    
+    def remove_comment(self, comment):
+        self.comments.remove(comment)
+        self.save()
+    
 class ForumComment(TextComment):
     
     subComments = models.ManyToManyField('ForumComment', related_name='Replies', blank=True)
+    
+    def add_comment(self, comment):
+        self.subComments.add(comment)
+        self.save()
+
+    def remove_comment(self, comment):
+        self.subComments.remove(comment)
+        self.save()
 
 class Forum(models.Model):
     class Meta:
@@ -19,3 +35,11 @@ class Forum(models.Model):
     title = models.CharField(max_length=30, blank=False, null=False)
     posts = models.ManyToManyField('ForumPost', related_name='Posts', blank=True)
     associatedWith = models.ForeignKey('Club', on_delete=models.CASCADE, blank=True)
+    
+    def add_post(self, post):
+        self.posts.add(post)
+        self.save()
+    
+    def remove_post(self, post):
+        self.posts.remove(post)
+        self.save()
