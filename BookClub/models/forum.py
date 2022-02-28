1dc204902c8ce19454fab6d9e38992748b182998
Jack Curtis <jack.curtis@kcl.ac.uk>
2022-02-28 02:00:02 +0000

    Add ForumPost, ForumComment, and Forum models

--- BookClub/models/forum.py
+++ BookClub/models/forum.py
@@ -1,5 +1,21 @@
 from django.db import models
 from BookClub.models import TextPost, RatedContent
+from BookClub.models.rated_content import TextComment
 
 class ForumPost(TextPost):
-    pass
+    class Meta:
+        ordering = ['-created_on']
+        
+    comments = models.ManyToManyField('ForumComment', related_name='Comments', blank=True)
+    
+class ForumComment(TextComment):
+    
+    subComments = models.ManyToManyField('ForumComment', related_name='Replies', blank=True)
+
+class Forum(models.Model):
+    class Meta:
+        unique_together = [['title','associatedWith']]
+        
+    title = models.CharField(max_length=30, blank=False, null=False)
+    posts = models.ManyToManyField('ForumPost', related_name='Posts', blank=True)
+    associatedWith = models.ForeignKey('Club', on_delete=models.CASCADE, blank=True)
