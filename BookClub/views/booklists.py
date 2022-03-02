d4407679ed6ccd647d9ac650cdd7597770fa0720
sebmalos <sebastian.malos@kcl.ac.uk>
2022-03-02 04:38:29 +0000

    Create edit booklist view
    Co-Authored-By: Raymond <78810792+pleofthegem@users.noreply.github.com>

--- BookClub/views/booklists.py
+++ BookClub/views/booklists.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
+from django.shortcuts import reverse, redirect
 
 from BookClub.models import User, BookList
 from BookClub.forms import CreateBookListForm
@@ -40,3 +41,44 @@ class CreateBookListView(LoginRequiredMixin, CreateView):
     def form_invalid(self, form):
         messages.add_message(self.request, messages.ERROR, "The data provided was invalid!")
         return super().form_invalid(form)
+
+class EditBookListView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = BookList
+    form_class = CreateBookListForm
+    template_name = 'edit_booklist.html'
+    context_object_name = 'booklist'
+    redirect_location = 'booklists_list'
+
+    def test_func(self):
+        try:
+            booklist = BookList.objects.get(pk=self.kwargs['booklist_id'])
+            user = User.objects.get(username=self.kwargs['username'])
+            if self.request.user == user:
+                return True
+            else:
+                messages.error(self.request, 'You are not the creator of this book list!')
+                return False
+        except:
+            messages.error(self.request, 'Book list not found!')
+            return False
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            return super(LoginRequiredMixin, self).handle_no_permission()
+        else:
+            return redirect(self.redirect_location, self.kwargs['username'])
+
+    def get_success_url(self):
+        """Return redirect URL after successful update."""
+        messages.success(self.request, "You have updated the book list!")
+        return reverse(self.redirect_location, kwargs={'username': self.kwargs['username']})
+
+    def get_object(self):
+        return BookList.objects.get(id=self.kwargs['booklist_id'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        booklist = self.get_object()
+        context['booklist'] = booklist
+        context['id'] = booklist.id
+        return context
