015b7ca2796641aed964f4eb1ff0ab4230cfc572
Amir Rahim <amir.rahim@kcl.ac.uk>
2022-03-02 20:37:45 +0000

    Reformat Booklist View

--- BookClub/views/booklists.py
+++ BookClub/views/booklists.py
@@ -7,6 +7,7 @@ from django.views.generic import ListView, CreateView
 from BookClub.models import User, BookList
 from BookClub.forms import CreateBookListForm
 
+
 class BooklistListView(ListView):
     http_method_names = ['get']
     model = BookList
@@ -15,16 +16,17 @@ class BooklistListView(ListView):
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs['username'])
-        return BookList.objects.filter(creator = user)
+        return BookList.objects.filter(creator=user)
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
         # Add in a QuerySet of all the books
-        creator = User.objects.get(username = self.kwargs['username'])
+        creator = User.objects.get(username=self.kwargs['username'])
         context['creator'] = creator
         context['self'] = self.request.user == creator
         return context
+
 
 class CreateBookListView(LoginRequiredMixin, CreateView):
     template_name = 'create_booklist.html'
@@ -34,7 +36,7 @@ class CreateBookListView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.creator = self.request.user
         response = super().form_valid(form)
-        messages.success(self.request, ('Successfully created a new booklist!'))
+        messages.success(self.request, 'Successfully created a new booklist!')
         return response
 
     def form_invalid(self, form):
