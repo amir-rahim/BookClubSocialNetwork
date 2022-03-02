dc1bba47a1b39bcf9e578facb223695aed441b94
Amir Rahim <amir.rahim@kcl.ac.uk>
2022-03-02 22:56:37 +0000

    Reformat Files

--- BookClub/management/commands/importbooks.py
+++ BookClub/management/commands/importbooks.py
@@ -6,45 +6,46 @@ from pandas import DataFrame
 import os
 import requests
 
+
 class Command(BaseCommand):
     """The database seeder."""
-        
+
     def add_arguments(self, parser):
         parser.add_argument('books', type=int, nargs='?', default=5)
-        
+
     def handle(self, *args, **options):
         file_path = "static/dataset/BX_Books.csv"
 
         # As we're loading a custom dataset, we need to define a reader. In the
         # movielens-100k dataset, each line has the following format:
         # 'user item rating timestamp', separated by '\t' characters.
-        file = open(file_path,'rb',0)
+        file = open(file_path, 'rb', 0)
 
-        data = DataFrame(pd.read_csv(file_path, header=0, encoding= "ISO-8859-1", sep=';'))
-        
+        data = DataFrame(pd.read_csv(file_path, header=0, encoding="ISO-8859-1", sep=';'))
+
         df_records = data.to_dict('records')
         percent = options.get('books', None)
-        
+
         model_instances = [Book(
-            title = record['Book-Title'],
-            ISBN = record['ISBN'],
-            author = record['Book-Author'],
-            publicationYear = self.cleanYear(record['Year-Of-Publication']),
-            publisher = record['Publisher'],
-            imageS = record['Image-URL-S'],
-            imageM = record['Image-URL-M'], 
-            imageL = record['Image-URL-L'],
+            title=record['Book-Title'],
+            ISBN=record['ISBN'],
+            author=record['Book-Author'],
+            publicationYear=self.cleanYear(record['Year-Of-Publication']),
+            publisher=record['Publisher'],
+            imageS=record['Image-URL-S'],
+            imageM=record['Image-URL-M'],
+            imageL=record['Image-URL-L'],
         ) for record in df_records]
-        count = int(len(model_instances)*(percent/100))
+        count = int(len(model_instances) * (percent / 100))
         random_sample = random.sample(model_instances, count)
         try:
             Book.objects.bulk_create(random_sample)
         except Exception as e:
             print(e)
-            
+
     def cleanYear(self, year):
         string = str(year)
         if string == "0":
             return "0001-01-01"
         else:
-            return string+"-01-01"
+            return string + "-01-01"
