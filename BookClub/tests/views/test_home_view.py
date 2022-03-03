fdfe12fd4cbca947567c1d984d63d6bd8618e494
Amir Rahim <amir.rahim@kcl.ac.uk>
2022-03-03 17:25:56 +0000

    Fix Home Page Links

--- BookClub/tests/views/test_home_view.py
+++ BookClub/tests/views/test_home_view.py
@@ -13,10 +13,9 @@ class HomeViewTestCase(TestCase):
 
     def test_get_home(self):
         response = self.client.get(self.url)
-
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
 
     def test_available_clubs_link_redirect(self):
         response = self.client.get(self.url)
-        self.assertContains(response, '<span>Join</span>')
+        self.assertContains(response, '<a class="card-footer-item" href="/club/">Join</a>')
