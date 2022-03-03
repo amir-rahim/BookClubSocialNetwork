d7a10c05acab3270c0e2a483adbd8ed2c026caa3
Raymond Chung <k20052048@kcl.ac.uk>
2022-03-03 20:25:09 +0000

    add
    add form for BookReviewComment
    
    Co-Authored-By: clerav <ravshanbek.rozukulov@kcl.ac.uk>

--- BookClub/forms/review.py
+++ BookClub/forms/review.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm, Textarea, ValidationError
 from django.core.validators import RegexValidator
 from django.test import tag
 from BookClub.models import User, Book, BookReview
+from BookClub.models.review import BookReviewComment
 
 
 class ReviewForm(ModelForm):
@@ -17,3 +18,14 @@ class ReviewForm(ModelForm):
         labels = {
             'rating': 'Rating'
         }
+
+class BookReviewCommentForm(ModelForm):
+    class Meta:
+        model = BookReviewComment
+
+        fields = ['content']
+        
+        widgets = {
+            'content': Textarea,
+        }
+        
