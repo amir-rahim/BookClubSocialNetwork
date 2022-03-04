e47df51d3ce681b26c7b233f0a54e990a6f42bc3
Musa Ghafoor <k20073759@kcl.ac.uk>
2022-03-04 00:44:28 +0000

    Create calendar model
    Co-Authored-By: sebmalos <91273529+sebmalos@users.noreply.github.com>

---
+++ BookClub/models/calendar.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
