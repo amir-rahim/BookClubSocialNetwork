311ec138d16e54aae3005202350d3f55e6176990
Jessy BRIARD <jessy.briard@kcl.ac.uk>
2022-03-08 00:30:43 +0100

    Merge main into ImportReviews

--- BookClub/models/review.py
+++ BookClub/models/review.py
@@ -4,12 +4,12 @@ from datetime import datetime
 from BookClub.models.book import Book
 
 class BookReview(models.Model):
-    
+
     class Meta:
         unique_together =['book', 'user']
-    
+
     RATINGS = [(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10)]
-    
+
     book = models.ForeignKey('Book', on_delete=models.CASCADE)
     rating = models.IntegerField(verbose_name='Ratings',choices=RATINGS, default=0, blank=False, null=False)
     review = models.CharField(verbose_name="Review:", max_length=1024,blank=True, null=False)
