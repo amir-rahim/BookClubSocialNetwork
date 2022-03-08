00a6b40a467e117894e224d65751f8aec79f61d0
Amir Rahim <amir.rahim@kcl.ac.uk>
2022-03-08 02:07:11 +0000

    Increase Coverage of Testing

--- BookClub/tests/views/club_views/test_club_dashboard_view.py
+++ BookClub/tests/views/club_views/test_club_dashboard_view.py
@@ -5,7 +5,8 @@ from django.urls import reverse
 from BookClub.tests.helpers import reverse_with_next
 from django.test import Client
 
-@tag('clubdashboard')
+
+@tag('club', 'club_dashboard')
 class ClubDashboardViewTest(TestCase):
     """Unit tests of the club page view."""
     fixtures = [
@@ -17,16 +18,22 @@ class ClubDashboardViewTest(TestCase):
     def setUp(self):
         self.user = User.objects.get(username="johndoe")
         self.jane = User.objects.get(username="janedoe")
-        self.jack = User.objects.get(username="jackdoe") 
+        self.jack = User.objects.get(username="jackdoe")
         self.club = Club.objects.get(name="Johnathan Club")
         self.url = reverse("club_dashboard", kwargs={"club_url_name": self.club.club_url_name})
         self.private_club = Club.objects.get(name="Jack Club")
         self.private_url = reverse("available_clubs")
-        
-
 
     def test_club_dashboard_url(self):
         self.assertEqual(self.url, f"/club/Johnathan_Club/")
+
+    def test_get_club_dashboard_not_logged_in(self):
+        url = reverse("club_dashboard", kwargs={"club_url_name": self.private_club.club_url_name})
+        redirect_url = reverse_with_next('login', url)
+        response = self.client.get(url)
+        self.assertRedirects(response, redirect_url,
+                             status_code=302, target_status_code=200, fetch_redirect_response=True
+                             )
 
     def test_club_dashboard_has_club_info(self):
         self.client.login(username=self.user.username, password="Password123")
@@ -35,7 +42,8 @@ class ClubDashboardViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "club_dashboard.html")
         self.assertContains(response, "Johnathan Club")
-        self.assertContains(response, "This is a very cool club that is owned by a certain Johnathan. Reading certain books...")
+        self.assertContains(response,
+                            "This is a very cool club that is owned by a certain Johnathan. Reading certain books...")
         self.assertContains(response, "Welcome to Johnathan&#x27;s club! We read the best books!!!")
         self.assertContains(response, "Don&#x27;t be annoying")
         self.assertContains(response, "Feb. 4, 2022")
@@ -66,14 +74,12 @@ class ClubDashboardViewTest(TestCase):
         response = self.client.get(reverse("club_dashboard", kwargs={"club_url_name": self.private_club.club_url_name}))
         self.assertRedirects(response, expected_url=reverse("available_clubs"), status_code=302, target_status_code=200)
 
-
     def test_private_club_member_can_view_dashboard(self):
         self.client.login(username=self.jane.username, password="Password123")
         url = reverse("club_dashboard", kwargs={"club_url_name": self.private_club.club_url_name})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "club_dashboard.html")
-
 
     def test_owner_has_admin_options(self):
         self.client.login(username=self.jane.username, password="Password123")
@@ -94,7 +100,7 @@ class ClubDashboardViewTest(TestCase):
         self.assertNotContains(response, "Club Administration")
         self.assertNotContains(response, "Manage Club")
         self.assertNotContains(response, "Club Settings")
-    
+
     def test_member_has_no_admin_options(self):
         self.client.login(username=self.jack.username, password="Password123")
         url = reverse("club_dashboard", kwargs={"club_url_name": self.club.club_url_name})
@@ -109,8 +115,8 @@ class ClubDashboardViewTest(TestCase):
         self.client.login(username=self.jack.username, password="Password123")
         response = self.client.get(reverse("club_dashboard", kwargs={"club_url_name": self.private_club.club_url_name}))
         self.assertRedirects(response, expected_url=reverse("available_clubs"), status_code=302, target_status_code=200)
-        
+
     def test_invalid_club(self):
         self.client.login(username=self.jack.username, password="Password123")
-        response = self.client.get(reverse("club_dashboard", kwargs={"club_url_name":'fakeclub'}))
+        response = self.client.get(reverse("club_dashboard", kwargs={"club_url_name": 'fakeclub'}))
         self.assertRedirects(response, expected_url=reverse("available_clubs"), status_code=302, target_status_code=200)
