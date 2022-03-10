8be9889800f9b7f3ead5632270a367f70a42ca97
Jessy BRIARD <jessy.briard@kcl.ac.uk>
2022-03-10 17:49:22 +0000

    Add methods to build and get a matrix of all books read by all users, computed from the Leave-One-Out trainset, to the EvaluationDataProvider class

--- RecommenderModule/evaluation_data_provider.py
+++ RecommenderModule/evaluation_data_provider.py
@@ -7,10 +7,12 @@ class EvaluationDataProvider:
     dataset = None
     loocv_trainset = None
     loocv_testset = None
+    read_books_all_users = {}
     
     def __init__(self, dataset):
         self.dataset = dataset
         self.build_loocv_datasets()
+        
         
     """Split the original dataset to build 'Leave-One-Out' train and test sets
         for evaluating top-N recommenders."""
@@ -25,3 +27,21 @@ class EvaluationDataProvider:
         the original dataset, using 'Leave-One-Out'."""
     def get_loocv_datasets(self):
         return (self.loocv_trainset, self.loocv_testset)
+    
+    
+    def build_read_books_all_users_dict(self):
+        self.read_books_all_users = {}
+        trainset = self.loocv_trainset
+        for inner_user_id, inner_item_id, rating in trainset.all_ratings():
+            print(f"{trainset.to_raw_uid(inner_user_id)} -> {trainset.to_raw_iid(inner_item_id)}")
+            raw_user_id = trainset.to_raw_uid(inner_user_id)
+            raw_item_id = trainset.to_raw_iid(inner_item_id)
+            try:
+                self.read_books_all_users[raw_user_id].append(raw_item_id)
+            except:
+                self.read_books_all_users[raw_user_id] = [raw_item_id]
+    
+    def get_read_books_all_users_dict(self):
+        if (self.read_books_all_users == {}):
+            self.build_read_books_all_users_dict()
+        return self.read_books_all_users
