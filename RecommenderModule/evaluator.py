10d66e8783a4658617dde68666ff06eabde0943b
Jessy BRIARD <jessy.briard@kcl.ac.uk>
2022-03-11 16:54:11 +0100

    Change left_out_test_set iterations to use raw user_id and book_id instead of inner uid and iid

--- RecommenderModule/evaluator.py
+++ RecommenderModule/evaluator.py
@@ -3,21 +3,22 @@
 def get_hit_rate(recommendations, left_out_test_set):
     hits = 0
     total = 0
-    
-    for left_out_item in left_out_test_set:
-        user_id = left_out_item[0]
-        book_id = left_out_item[1]
-        
+
+    for inner_user_id, inner_item_id, rating in left_out_test_set:
+        user_id = left_out_test_set.to_raw_uid(inner_user_id)
+        book_id = left_out_test_set.to_raw_iid(inner_item_id)
+
         # We increment the number of hits if the book has been recommended to the user
         if (book_id in recommendations[user_id]):
             hits += 1
-            
+
         total += 1
-        
+
     # Check whether can remove 'total' increment in loop (TO BE REMOVED)
     print(total == len(left_out_test_set))
-    
+
     return hits / total
+
 
 """Get the average reciprocal (weighted) hit-rate of an algorithm, given the
     recommendations produced from the dataset's LOOCV train set and the
@@ -25,11 +26,11 @@ def get_hit_rate(recommendations, left_out_test_set):
 def get_average_reciprocal_hit_rate(recommendations, left_out_test_set):
     hits = 0
     total = 0
-    
-    for left_out_item in left_out_test_set:
-        user_id = left_out_item[0]
-        book_id = left_out_item[1]
-        
+
+    for inner_user_id, inner_item_id, rating in left_out_test_set:
+        user_id = left_out_test_set.to_raw_uid(inner_user_id)
+        book_id = left_out_test_set.to_raw_iid(inner_item_id)
+
         # We increment the value of 'hits' if the book has been recommended to the user,
         # with a weighting corresponding to its rank in the recommendations
         rank = 1
@@ -38,10 +39,10 @@ def get_average_reciprocal_hit_rate(recommendations, left_out_test_set):
                 hits += (1/rank)
                 break
             rank += 1
-            
+
         total += 1
-        
+
     # Check whether can remove 'total' increment in loop (TO BE REMOVED)
     print(total == len(left_out_test_set))
-    
+
     return hits / total
