f2b71537432e9944b3454369e2db994e1ac47b66
Jack Curtis <jack.curtis@kcl.ac.uk>
2022-03-12 13:48:32 +0000

    Fix html, and add testing for addbookform

---
+++ BookClub/tests/forms/test_add_book_form.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.test import TestCase, tag
+from BookClub.models import Book, BookList
+from BookClub.forms import AddBookForm
+
+
+@tag('booklist', 'book', 'addbook')
+class AddBookFormTestCase(TestCase):
+    fixtures = [
+        "BookClub/tests/fixtures/default_users.json",
+        "BookClub/tests/fixtures/default_books.json",
+        "BookClub/tests/fixtures/booklists.json",
+    ]
+    """Unit tests for Add Book Form"""
+
+    def setUp(self):
+        self.book = Book.objects.get(pk=2)
+        self.booklist = BookList.objects.get(pk=1)
+        self.form_input = {
+            'book': 2,
+            'booklist': 1
+        }
+
+    def test_valid_add_book_form(self):
+        form = AddBookForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_form_has_necessary_fields(self):
+        form = AddBookForm(data=self.form_input)
+        self.assertIn('book', form.fields)
+        self.assertIn('booklist', form.fields)
+
+    def test_form_must_save_correctly(self):
+        form = AddBookForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form['book'].value(), self.book.id)
+        self.assertEqual(form['booklist'].value(), self.booklist.id)
