77e806867484278d5a1782728ca20d97c94185b5
Jack Curtis <jack.curtis@kcl.ac.uk>
2022-03-16 17:11:28 +0000

    Add clean function to require a book if meeting type is book

--- BookClub/forms/meeting.py
+++ BookClub/forms/meeting.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, Textarea, TextInput
+from django.forms import ModelForm, Textarea, TextInput, ValidationError
 
 from BookClub.models.meeting import Meeting
 
@@ -19,3 +19,12 @@ class MeetingForm(ModelForm):
             'description': Textarea,
             'meeting_time': TextInput(attrs={'placeholder': 'YYYY/MM/DD HH:MM:SS'})
         }
+        
+    def clean(self):
+        super().clean()
+        meeting_type = self.cleaned_data.get('type')
+        if meeting_type == Meeting.MeetingType.BOOK:
+            book = self.cleaned_data.get('book')
+            if book is None:
+                raise ValidationError("A book meeting needs a book selected!")
+        
