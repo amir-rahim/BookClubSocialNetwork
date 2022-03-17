ce83b3766092e51c1ce09140360a253162f31487
Jack Curtis <jack.curtis@kcl.ac.uk>
2022-03-17 20:30:37 +0000

    Refactor naming and structure of book_search partials

--- BookClub/forms/widgets.py
+++ BookClub/forms/widgets.py
@@ -3,4 +3,4 @@ from django.forms.widgets import Input
 
 class BookSelectorInput(Input):
     input_type = 'hidden'
-    template_name = 'partials/book_search_partial.html/'
+    template_name = 'partials/book_search_select.html/'
