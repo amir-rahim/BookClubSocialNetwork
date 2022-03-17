eb4cabea3221beceb7ee51d827557a7f64c0d1c5
Jack Curtis <jack.curtis@kcl.ac.uk>
2022-03-17 20:31:08 +0000

    Replace library search with ajax enabled search, update view to accomodate

--- BookClub/views/async_views/book_search.py
+++ BookClub/views/async_views/book_search.py
@@ -4,15 +4,16 @@ from django.db.models import Q
 from django.template.loader import render_to_string
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from BookClub.models import Book
+from BookClub.models.booklist import BookList
 
 class BookSearchView(TemplateView):
     
     def get(self, request, *args, **kwargs):
         url_parameter = request.GET.get('q')
+        select = request.GET.get('select')
         page = request.GET.get('page', 1)
         context = {}
         books = None
-        
         
         if url_parameter:
             query = url_parameter
@@ -21,18 +22,43 @@ class BookSearchView(TemplateView):
                     publisher__icontains=query)
             )
             
-            paginator = Paginator(books, 5)
+            paginate_by = 10
+            
+            if select:
+                paginate_by = 5
+            
+            user = self.request.user
+            if not user.is_anonymous:
+                context['lists'] = BookList.objects.filter(creator=user)
+                context['user'] = user
+            else:
+                context['lists'] = None
+                context['user'] = None
+    
+            paginator = Paginator(books, paginate_by)
             
             try:
                 books = paginator.page(page)
             except PageNotAnInteger:
+                print("not int")
                 books = paginator.page(1)
             except EmptyPage:
+                print("empty")
                 books = paginator.page(paginator.num_pages)
-            
+        else:
+            books = Book.objects.all()
         context['books'] = books
+        
         if request.is_ajax():
             html = render_to_string(
-                template_name='partials/partial_books_list.html', context={'books': books})
+                template_name=self.get_template_names()[0], context=context)
             data_dict = {"html_from_view" : html}
             return JsonResponse(data=data_dict, safe=False)
+        
+    def get_template_names(self):
+        select = self.request.GET.get('select')
+        
+        if select:
+            return ['partials/book_select_list.html']
+        
+        return ['partials/book_search_list.html']
