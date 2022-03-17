48f84ffb7e368aecb25180222a1fb1f45f2989a1
clerav <ravshanbek.rozukulov@kcl.ac.uk>
2022-03-17 17:02:10 +0000

    Add helper functions to review models

--- BookClub/models/review.py
+++ BookClub/models/review.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from BookClub.models.rated_content import *
 
 class BookReview(TextPost):
@@ -10,7 +11,26 @@ class BookReview(TextPost):
     book = models.ForeignKey('Book', on_delete=models.CASCADE)
     book_rating = models.IntegerField(verbose_name='Rating', choices=RATINGS, default=0, blank=False, null=False)
 
+    def get_comments(self):
+        return self.bookreviewcomment_set.all()
+
+    def get_delete_url(self):
+        return reverse('delete_review', kwargs={'book_id': self.book.pk})
+
+    def __str__(self):
+        return f'{self.book_rating}/10 rating & review by {str(self.creator)} on "{str(self.book)}"'
 
 class BookReviewComment(TextComment):
 
     book_review = models.ForeignKey('BookReview', blank = False, null = False, on_delete=models.CASCADE)
+
+    def get_delete_url(self):
+        kwargs={
+            'book_id': self.book_review.book.pk,
+            'review_id': self.book_review.pk,
+            'comment_id': self.pk
+        }
+        return reverse('delete_review_comment', kwargs=kwargs)
+
+    def __str__(self):
+        return f'Comment by {str(self.creator)} on {str(self.book_review)}'
