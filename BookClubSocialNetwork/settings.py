f73f32ec51228f6e8a3402ced804b7e627ba91fe
Musa Ghafoor <k20073759@kcl.ac.uk>
2022-03-18 23:37:24 +0000

    Update settings for SMTP configuration
    Co-Authored-By: sebmalos <91273529+sebmalos@users.noreply.github.com>

--- BookClubSocialNetwork/settings.py
+++ BookClubSocialNetwork/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-%af@p+7sw$$0+onbzcj6xugj7v2f*k=oco%h@&wsadm@3wglvk
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -75,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'BookClubSocialNetwork.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -85,7 +82,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -119,13 +114,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -145,30 +139,29 @@ AUTH_USER_MODEL = 'BookClub.User'
 try:
     if '/app' in os.environ['HOME']:
         import django_heroku
+
         django_heroku.settings(locals())
 except:
     if '/app' in os.path.expanduser('~'):
         import django_heroku
+
         django_heroku.settings(locals())
 
 # SMTP Configuration
 # For Django Email Backend
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-# EMAIL_HOST_USER = os.environ.get('')
-# EMAIL_HOST_PASSWORD = os.environ.get('')  # os.environ['password_key'] suggested
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = os.environ.get('')
+EMAIL_HOST_PASSWORD = os.environ.get('')
 EMAIL_USE_TLS = True
 
 DEFAULT_FROM_EMAIL = 'noreply<no_reply@domain.com>'
 
 EXPIRE_AFTER = "1h" # Will expire after one hour from link generation
 
-HTML_MESSAGE_TEMPLATE = 'verification_email.html'
+HTML_MESSAGE_TEMPLATE = 'account_verification/verification_email.html'
 VERIFICATION_SUCCESS_TEMPLATE = None
-VERIFICATION_FAILED_TEMPLATE = 'verification_email_failed.html'
-REQUEST_NEW_EMAIL_TEMPLATE = 'verification_email_request.html'
-LINK_EXPIRED_TEMPLATE = 'verification_email_expired.html'
+VERIFICATION_FAILED_TEMPLATE = 'account_verification/verification_email_failed.html'
+REQUEST_NEW_EMAIL_TEMPLATE = 'account_verification/verification_email_request.html'
+LINK_EXPIRED_TEMPLATE = 'account_verification/verification_email_expired.html'
