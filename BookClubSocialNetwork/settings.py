a825c8b0f5060bc6219ea367ecfb1729436bc501
Amir Rahim <amir.rahim@kcl.ac.uk>
2022-03-19 00:21:49 +0000

    Add Default Values to OS Env

--- BookClubSocialNetwork/settings.py
+++ BookClubSocialNetwork/settings.py
@@ -158,8 +158,8 @@ environ.Env.read_env()
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env('EMAIL_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
+EMAIL_HOST_USER = os.getenv('EMAIL_USER', env('EMAIL_USER'))
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD', env('EMAIL_PASSWORD'))
 
 EMAIL_USE_TLS = True
 
