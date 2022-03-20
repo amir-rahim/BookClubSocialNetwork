fe43038daffe9a9b04a37111824716e21694f344
Jack Curtis <jack.curtis@kcl.ac.uk>
2022-03-20 22:16:44 +0000

    Fix phrasing

--- BookClub/tests/views/relationship_views/test_follower_following.py
+++ BookClub/tests/views/relationship_views/test_follower_following.py
@@ -1,3 +1,4 @@
+from click import password_option
 from django.test import TestCase, tag
 from django.urls import reverse
 from BookClub.models import User, UserToUserRelationship
@@ -18,8 +19,43 @@ class FollowingFolloweeTestCase(TestCase):
     def setUp(self):
         self.user1 = User.objects.get(pk=1)
         self.user2 = User.objects.get(pk=2)
-        self.rel1 = UserToUserRelationship.objects.get(pk=1)
+        self.following_relation = UserToUserRelationship.objects.get(pk=1)
+        self.follower_relation = UserToUserRelationship.objects.get(pk=2)
         self.url = reverse('user_following', kwargs={'username':self.user1.username})
         
     def test_url(self):
         self.assertEqual(self.url, '/profile/johndoe/following/')
+        
+    def test_no_followers(self):
+        self.following_relation.delete()
+        self.follower_relation.delete()
+        self.client.login(username=self.user1.username, password="Password123")
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'following_followers.html')
+        self.assertContains(response,"There are no followers for this user.")
+        
+    def test_1_follower(self):
+        self.following_relation.delete()
+        self.client.login(username=self.user1.username, password="Password123")
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'following_followers.html')
+        self.assertContains(response,"1 follower")
+        self.assertContains(response, self.user2.username)
+        
+    def test_no_followee(self):
+        self.following_relation.delete()
+        self.follower_relation.delete()
+        self.client.login(username=self.user1.username, password="Password123")
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'following_followers.html')
+        self.assertContains(response, "This user isn't following anyone")
+        
+
+    def test_1_followee(self):
+        self.follower_relation.delete()
+        self.client.login(username=self.user1.username, password="Password123")
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'following_followers.html')
+        self.assertContains(response, "1 following")
+        self.assertContains(response, self.user2.username)
+        
