f2783b1065383098e42be160ac01606f1e9ed275
Amir Rahim <amir.rahim@kcl.ac.uk>
2022-03-21 23:49:24 +0000

    Remove Club Settings Button

--- BookClub/tests/views/club_views/test_member_list_view.py
+++ BookClub/tests/views/club_views/test_member_list_view.py
@@ -84,7 +84,6 @@ class MemberListTestCase(TestCase):
         self.assertTemplateUsed(response, 'club_members.html')
         self.assertContains(response, "Club Administration")
         self.assertContains(response, "Manage Club")
-        self.assertContains(response, "Club Settings")
 
     def test_mod_has_no_admin_options(self):
         self.client.login(username=self.user.username, password="Password123")
@@ -93,7 +92,6 @@ class MemberListTestCase(TestCase):
         self.assertTemplateUsed(response, 'club_members.html')
         self.assertNotContains(response, "Club Administration")
         self.assertNotContains(response, "Manage Club")
-        self.assertNotContains(response, "Club Settings")
 
     def test_member_has_no_admin_options(self):
         self.client.login(username=self.jack.username, password="Password123")
@@ -102,7 +100,6 @@ class MemberListTestCase(TestCase):
         self.assertTemplateUsed(response, 'club_members.html')
         self.assertNotContains(response, "Club Administration")
         self.assertNotContains(response, "Manage Club")
-        self.assertNotContains(response, "Club Settings")
 
     def test_can_see_owner(self):
         self.client.login(username=self.user.username, password="Password123")
