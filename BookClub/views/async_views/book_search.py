43060a75ce81648783644104f75716c283fb6535
Jack Curtis <jack.curtis@kcl.ac.uk>
2022-03-23 20:43:51 +0000

    Partially implement query builder

--- BookClub/views/async_views/book_search.py
+++ BookClub/views/async_views/book_search.py
@@ -2,9 +2,10 @@ from django.http import JsonResponse
 from django.views.generic import TemplateView  
 from django.db.models import Q
 from django.template.loader import render_to_string
+from django.contrib.contenttypes.models import ContentType
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from BookClub.models import Book
-from BookClub.models.booklist import BookList
+from django.contrib.postgres.search import SearchVector
+from BookClub.models import *
 
 class BookSearchView(TemplateView):
     
@@ -15,32 +16,30 @@ class BookSearchView(TemplateView):
         select = request.GET.get('select')
         page = request.GET.get('page', 1)
         query = request.GET.get('q', None)
+        content_type = request.GET.get('c', None)
         
-        books = self.get_queryset(query)
+        objects = self.get_queryset(content_type=content_type, query=query)
         
         if select:
             self.paginate_by = 5
             
-        context['page_obj'] = self.get_pagination(books, page)
+        context['page_obj'] = self.get_pagination(objects, page)
         context['lists'] = None
         
         user = self.request.user
         if not user.is_anonymous:
             context['lists'] = BookList.objects.filter(creator=user)        
 
-        
         html = render_to_string(
-            template_name=self.get_template_names()[0], context=context, request=request)
+            template_name=self.get_template_names(), context=context, request=request)
         data_dict = {"html_from_view" : html}
         return JsonResponse(data=data_dict, safe=False)
         
-    def get_queryset(self, query=None):
-        if query is not None:
-            return Book.objects.filter(
-                Q(title__icontains=query) | Q(author__icontains=query) | Q(
-                    publisher__icontains=query)
-            )
-        return Book.objects.all()
+    def get_queryset(self, query=None, content_type = None):
+        #content_type = ContentType.objects.get(pk=content_type)
+        model = Book
+        Qs = self.build_query(model, query)
+        return model.objects.filter(Qs)
     
     def get_pagination(self, object_list, page=1):
         paginator = Paginator(object_list, self.paginate_by)
@@ -59,3 +58,24 @@ class BookSearchView(TemplateView):
             return ['partials/book_select_list.html']
         
         return ['partials/book_search_list.html']
+    
+    def build_query(self, model, query):
+        q_objects = Q()
+        if(issubclass(model, UserCreatedObject)):
+            q_objects.add(Q(creator__username__icontains=query), Q.OR)
+        if(issubclass(model, BookList)):
+            q_objects.add(Q(title__icontains=query), Q.OR)
+            q_objects.add(Q(description__icontains=query), Q.OR)
+        if(issubclass(model, ClubMembership)):
+            q_objects.add(Q(user__username__icontains=query), Q.OR)
+            q_objects.add(Q(club__name__icontains=query), Q.OR)
+        if(issubclass(model, Club)):
+            q_objects.add(Q(club__name__icontains=query), Q.OR)
+            q_objects.add(Q(description__icontains=query), Q.OR)
+            q_objects.add(Q(tagline__icontains=query), Q.OR)
+            q_objects.add(Q(rules__icontains=query), Q.OR)
+        if(issubclass(model, Book)):
+            q_objects.add(Q(title__icontains=query), Q.OR)
+            q_objects.add(Q(author__icontains=query), Q.OR)
+            q_objects.add(Q(publisher__icontains=query), Q.OR)
+        return q_objects
