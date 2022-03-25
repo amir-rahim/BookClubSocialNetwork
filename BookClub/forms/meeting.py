deb0c86468a5f5ab91d8ae21ce802f4dd2142eb5
Musa Ghafoor <k20073759@kcl.ac.uk>
2022-03-25 01:11:57 +0000

    Update meeting model to include end time
    Co-Authored-By: sebmalos <91273529+sebmalos@users.noreply.github.com>

--- BookClub/forms/meeting.py
+++ BookClub/forms/meeting.py
@@ -1,5 +1,5 @@
 from django.forms import HiddenInput, ModelForm, Textarea, TextInput, ValidationError
-from BookClub.forms.widgets import BookSelectorInput,DateTimePickerInput
+from BookClub.forms.widgets import BookSelectorInput, DateTimePickerInput
 from BookClub.models.meeting import Meeting
 
 
@@ -10,6 +10,7 @@ class MeetingForm(ModelForm):
             'title',
             'description',
             'meeting_time',
+            'meeting_end_time',
             'location',
             'type',
             'book',
@@ -17,15 +18,19 @@ class MeetingForm(ModelForm):
 
         widgets = {
             'description': Textarea,
-            'book' : BookSelectorInput(),
-            'meeting_time': DateTimePickerInput
+            'book': BookSelectorInput(),
+            'meeting_time': DateTimePickerInput,
+            'meeting_end_time': DateTimePickerInput
         }
-        
+
     def clean(self):
         super().clean()
         meeting_type = self.cleaned_data.get('type')
+        meeting_time = self.cleaned_data.get('meeting_time')
+        meeting_end_time = self.cleaned_data.get('meeting_end_time')
         if meeting_type == Meeting.MeetingType.BOOK:
             book = self.cleaned_data.get('book')
             if book is None:
                 self.add_error('book', "Book is required when creating a book meeting")
-        
+        if meeting_time > meeting_end_time:
+            self.add_error('meeting_end_time', 'Meeting end time must be after the meeting time.')
