6187f4d0fba86abf4ff1a11305ed736a6f293224
Jack Curtis <jack.curtis@kcl.ac.uk>
2022-03-25 11:01:07 +0000

    Add context processor

--- BookClubSocialNetwork/settings.py
+++ BookClubSocialNetwork/settings.py
@@ -67,7 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'BookClub.form_processor.ForumPostForm'
+                'BookClub.form_processor.ForumPostForm',
+                'BookClub.form_processor.BooklistContext'
             ],
         },
     },
