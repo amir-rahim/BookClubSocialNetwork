4060ea69836e86e95d1d99d0043529c2e32f155a
Jack Curtis <jack.curtis@kcl.ac.uk>
2022-03-26 16:01:10 +0000

    Simplify view

--- BookClub/views/async_views/book_search.py
+++ BookClub/views/async_views/book_search.py
@@ -5,12 +5,13 @@ from django.template.loader import render_to_string
 from django.contrib.contenttypes.models import ContentType
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from BookClub.models import *
-from BookClub.views.async_views.search_query_builder import SearchQueries
+from BookClub.views.async_views.search_query_builder import ClubQuery, UserQuery, BookQuery, BookListQuery
 
 
 class SearchView(TemplateView):
 
     paginate_by = 10
+    search_queries = [ClubQuery, BookQuery, UserQuery, BookListQuery]
 
     def get(self, request, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
@@ -32,8 +33,6 @@ class SearchView(TemplateView):
 
     def get_queryset(self, query="", content_type=None):
         model = content_type.model_class()
-        if query == "":
-            return model.objects.all()
         Qs = self.get_query(model, query)
         obs = model.objects.filter(Qs)
         return obs
@@ -70,7 +69,7 @@ class SearchView(TemplateView):
 
     def get_query(self, model, query):
         q_objects = Q()
-        for query_class in SearchQueries:
-            q_objects = query_class.value(
-                query=query, q_objects=q_objects, model=model,).build_query(user=self.request.user)
+        for query_class in self.search_queries:
+            q_objects = query_class(
+                query=query, q_objects=q_objects, model=model).build_query(user=self.request.user)
         return q_objects
