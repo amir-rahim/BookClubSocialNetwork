81461df436b0d2adbf587a9de6aa929ed0877eff
Zafi Shah <shah.zafira@gmail.com>
2022-03-26 14:30:40 +0000

    Add tests for featured books
    Co-Authored-By: k20036179 <80788080+k20036179@users.noreply.github.com>

--- BookClub/tests/views/club_views/test_club_dashboard_view.py
+++ BookClub/tests/views/club_views/test_club_dashboard_view.py
@@ -2,7 +2,7 @@
 from django.test import TestCase, tag
 from django.urls import reverse
 
-from BookClub.models import Club, User
+from BookClub.models import Club, User, FeaturedBooks, Book
 from BookClub.tests.helpers import reverse_with_next
 
 
@@ -12,7 +12,8 @@ class ClubDashboardViewTest(TestCase):
     fixtures = [
         "BookClub/tests/fixtures/default_users.json",
         "BookClub/tests/fixtures/default_clubs.json",
-        "BookClub/tests/fixtures/default_memberships.json"
+        "BookClub/tests/fixtures/default_memberships.json",
+        'BookClub/tests/fixtures/default_books.json',
     ]
 
     def setUp(self):
@@ -23,6 +24,8 @@ class ClubDashboardViewTest(TestCase):
         self.url = reverse("club_dashboard", kwargs={"club_url_name": self.club.club_url_name})
         self.private_club = Club.objects.get(name="Jack Club")
         self.private_url = reverse("available_clubs")
+        self.book = Book.objects.get(pk=1)
+        self.featured_books = FeaturedBooks.objects.create(club = self.club, book = self.book, reason = "Great book")
 
     def test_club_dashboard_url(self):
         self.assertEqual(self.url, f"/club/Johnathan_Club/")
@@ -112,6 +115,15 @@ class ClubDashboardViewTest(TestCase):
         self.client.login(username=self.jack.username, password="Password123")
         response = self.client.get(reverse("club_dashboard", kwargs={"club_url_name": self.private_club.club_url_name}))
         self.assertRedirects(response, expected_url=reverse("available_clubs"), status_code=302, target_status_code=200)
+
+    def test_club_dashboard_has_featured_books(self):
+        self.client.login(username=self.user.username, password="Password123")
+        url = reverse("club_dashboard", kwargs={"club_url_name": self.club.club_url_name})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "club_dashboard.html")
+        self.assertContains(response, "Classical Mythology")
+        self.assertContains(response, "Mark P. O. Morford")
 
     def test_invalid_club(self):
         self.client.login(username=self.jack.username, password="Password123")
