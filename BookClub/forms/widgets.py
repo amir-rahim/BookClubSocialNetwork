dc05ddd522136a0de27593834517070816de2a48
clerav <ravshanbek.rozukulov@kcl.ac.uk>
2022-03-29 21:39:23 +0100

    deleted unused class

--- BookClub/forms/widgets.py
+++ BookClub/forms/widgets.py
@@ -6,26 +6,16 @@ from django.contrib.contenttypes.models import ContentType
 class BookSelectorInput(Input):
     input_type = 'hidden'
     template_name = 'partials/book_search_select.html/'
-    
-    def get_context(self, name, value, attrs):
-        context = super().get_context(name, value, attrs)
-        context['content_type'] = ContentType.objects.get(app_label="BookClub", model="book").pk
-        return context
-
-class BookCheckInput(Input):
-    input_type = 'hidden'
-    template_name = 'partials/book_search_check.html/'
 
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
         context['content_type'] = ContentType.objects.get(app_label="BookClub", model="book").pk
         return context
-    
 
 
 class DateTimePickerInput(DateTimeInput):
     input_type = 'datetime'
-    
+
 
 class DatePickerInput(DateInput):
     input_type = 'date'
