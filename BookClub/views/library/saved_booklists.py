9896186956f29c7f2e8dbc4ffc91b4ae78cc3e2b
Marsha Lau <k20036179@kcl.ac.uk>
2022-04-01 18:42:54 +0100

    Fix tests for remove saved booklist

--- BookClub/views/library/saved_booklists.py
+++ BookClub/views/library/saved_booklists.py
@@ -85,7 +85,7 @@ class RemoveSavedBookListView(LoginRequiredMixin, View):
     redirect_location = 'saved_booklists'
 
     def get(self, *args, **kwargs):
-        return redirect(self.redirect_location, booklist_id=self.kwargs['booklist_id'])
+        return redirect(self.redirect_location)
 
     def is_actionable(self, booklist):
         """Check if user can save the book list."""
@@ -107,10 +107,9 @@ class RemoveSavedBookListView(LoginRequiredMixin, View):
 
         try:
             booklist = BookList.objects.get(id=kwargs['booklist_id'])
-            creator = booklist.creator
         except:
             messages.error(self.request, "Error, invalid booklist.")
-            return redirect(self.redirect_location, booklist_id=self.kwargs['booklist_id'])
+            return redirect(self.redirect_location)
 
         if self.is_actionable(booklist):
             self.action(booklist)
