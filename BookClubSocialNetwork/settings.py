09504f20acbdaeb665bdd3a6b87614e3905870fd
Amir Rahim <amir.rahim@kcl.ac.uk>
2022-04-04 00:17:43 +0100

    Update Debug back

--- BookClubSocialNetwork/settings.py
+++ BookClubSocialNetwork/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.herokuapp.com']
 
