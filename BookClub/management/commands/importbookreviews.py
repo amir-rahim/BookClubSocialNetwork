f764798901d2c86f0893a78e9a9ddf16b725811b
Jack Curtis <jack.curtis@kcl.ac.uk>
2022-04-03 17:09:41 +0100

    optimise load command

--- BookClub/management/commands/importbookreviews.py
+++ BookClub/management/commands/importbookreviews.py
@@ -4,7 +4,7 @@ import pandas as pd
 import time
 from pandas import DataFrame
 from BookClub.models import User, BookReview, Book
-
+from faker import Faker
 
 class Command(BaseCommand):
     """The database seeder."""
@@ -31,26 +31,41 @@ class Command(BaseCommand):
         users = User.objects.all().only('id')
         len(books)
         len(users)
-        i = 0
-        model_instances = []
-        for book in books:
-            rows = data.loc[(data['ISBN'] == book.ISBN)]
-            records = rows.to_dict(orient="records")
-            model_instances.extend(self.process_records(records, book, users))
+        books = Book.objects.all().only('ISBN')
+        users = User.objects.all().only('id')
+        print(books.values_list('ISBN',flat=True))
+        rows = data[data['ISBN'].isin(
+            books.values_list('ISBN',flat=True))]
+        records = rows.to_dict('records')
+        model_instances = self.process_records(records, books, users)
         return model_instances
-            
+    
         
-    def process_records(self, records, book, users):
+
+    def process_records(self, records, books, users):
         reviews = []
+        self.faker = Faker()
+        book_error = 0
         for record in records:
+            username = self.faker.unique.user_name()
+            email = self.faker.unique.email()
             try:
+                user = User.objects.get_or_create(id=record['User-ID'], defaults={
+                                                  'username': username, 'email': email, 'password': 'pbkdf2_sha256$260000$qw2y9qdBlYmFUZVdkUqlOO$nuzhHvRnVDDOAo70OL14IEqk+bASVNTLjWS1N+c40VU='})[0]
                 b = BookReview(
-                    book=book,
-                    creator=users.get(id=record['User-ID']),
-                    book_rating=record['Book-Rating']
+                    book=books.get(ISBN=record['ISBN']),
+                    creator=user,
+                    book_rating=record['Book-Rating'],
+                    title=self.faker.sentence(nb_words=3),
+                    content=self.faker.paragraph(nb_sentences=3)
                 )
                 reviews.append(b)
+                if len(reviews) % 10000 == 0:
+                    print(str(len(reviews)) + " found so far")
             except Exception as e:
+                book_error += 1
                 continue
 
+        print(book_error)
+        
         return reviews
